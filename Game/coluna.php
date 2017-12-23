a4f9d16019bde6c114ff0c35594c5e1e4092b90c
Matheus Silva Freitas <msformigteen@live.com>
2017-12-23 19:55:42 -0300

    Arquivos Iniciais

---
+++ Game/coluna.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+  1 - Importe o arquivo "funcaoColuna.php" utilizando o require_once.
+
+  2 - Crie um Formulario que passará os dados via POST para resultado.php
+
+  3 - Dentro do formulário, chame a função "escreveInput" passando como parâmetro o número de jogadores que chegou nessa
+  página via GET.
+
+  4 - Antes de finalizar o formulário, crie um botão de Submit, porque a nossa função acima só cria os Inputs.
---
+++ Game/funcaoCheck.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    1 - Crie uma Função "analise" que recebe uma variável.
+
+      Dica: Funções em php têm esse tipo de estrutura:
+
+        function retornaVariavel ($entrada) {
+
+          if ( $entrada == 0 ) {
+            return 0;
+          } else {
+            return $entrada;
+          }
+        }
+
+        Mais detelhes em: https://secure.php.net/manual/pt_BR/language.functions.php
+
+    2 - Se a variável passada para função "analise" for maior que 10, a função deve retornar "false", caso contrário "true".
+*/
